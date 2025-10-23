64773093304bca8bc098ed1a8d37a8271c7b4b16
arsa040808 <arzha320@gmail.com>
2025-10-23 13:41:35 +0700

    Update API URL and modify store context

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "sixlicious-backend-production.up.railway.app/api";
+    const url = import.meta.env.VITE_API_URL;
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([])
 
